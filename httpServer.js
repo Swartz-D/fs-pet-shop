618848d2bfabb7a8d7e41c71d40bc0a741b2ab19
Dustin Swartzbaugh <dustin.swartzbaugh@gmail.com>
2022-10-28 01:06:07 -0400

    new commit

--- httpServer.js
+++ httpServer.js
@@ -9,18 +9,18 @@ const server = http.createServer((req, res)=>{
   switch(req.method){
     case 'GET':
       let url = req.url.split('/');
+      console.log(req.url);
       console.log('index',url[2]);
       let i = '/'+url[2];
       console.log('this is i', i);
+      console.log(typeof i);
       switch(req.url){
-        default:
+        case !'/pets':
           res.writeHead(404, {'Content-Type':'text/plain'});
           res.write('Not Found');
           res.end();
           break;
-
-        case '/pets':
-
+        case '/pets'+i:
           fs.readFile('pets.json', 'utf8', (err,data)=>{
             if(err){
               throw err;
@@ -28,6 +28,23 @@ const server = http.createServer((req, res)=>{
               const pets = JSON.parse(data);
               console.log(pets);
               var petsJSON = JSON.stringify(pets);
+              var petJSON = petsJSON[i];
+          res.writeHead(200, {'Content-Type':'application/json'});
+          res.write(petJSON)
+          res.end();
+            }})
+        case '/pets':
+
+          fs.readFile('pets.json', 'utf8', (err,data)=>{
+            if(err){
+              throw err;
+            } else {
+              const pets = JSON.parse(data);
+              console.log('pets i',pets[0]);
+              console.log('type pets i',typeof pets[0]);
+              var petsJSON = JSON.stringify(pets[0]);
+              console.log('petsJSON',typeof petsJSON);
+              console.log('petsJSON i', petsJSON);
           res.writeHead(200, {'Content-Type':'application/json'});
           res.write(petsJSON)
           res.end();
